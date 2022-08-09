28533c002a793debf5a5ee0edc5860c5f1c2f959
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-08-09 11:36:42 +0000

    add test for single DOM element

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,3 +1,13 @@
-// const buttonClick = require("../button");
+const { TestScheduler } = require("jest");
+const buttonClick = require("../button");
 
-// describe("DOM tests")
+beforeEach(() => {
+    document.body.innerHTML = "<p id='par'></p>";
+});
+
+describe("DOM tests", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+});
