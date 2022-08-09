679625d11bff153a46d1dfef4353bec36e38f049
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-08-09 12:57:24 +0000

    add whole html to mock dom

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -2,7 +2,11 @@ const { TestScheduler } = require("jest");
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
@@ -10,4 +14,7 @@ describe("DOM tests", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
     });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    })
 });
